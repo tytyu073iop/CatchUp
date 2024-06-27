a4d6512bdbd843009baa0009c573767833118ba3
ilya <ilya.biryuk2005@icloud.com>
2024-06-27 08:32:05 +0300

    Переименовал файлы

--- main(menu).py
+++ main(menu).py
@@ -1,17 +1,17 @@
 import pygame
 import pygame_menu
-import main
+import game
 
 pygame.init()
 surface = pygame.display.set_mode((1000, 1000))
 menu = pygame_menu.Menu('Catch Up!', 1000, 1000)
 def FSChange(state, **kwargs):
-    main.IsFullScreen = state
-    print("switched: Full screen", main.IsFullScreen, state)
+    game.IsFullScreen = state
+    print("switched: Full screen", game.IsFullScreen, state)
 menu.add.toggle_switch('Полный экран', False, onchange=FSChange)
 def VChange(state, **kwargs):
-    main.IsVSync = state
+    game.IsVSync = state
 menu.add.toggle_switch('VSync', False, onchange=VChange)
 pygame.display.set_caption("Догонялки-Убегалки")
-menu.add.button('two players', main.game, False)
+menu.add.button('two players', game.game, False)
 menu.mainloop(surface)
